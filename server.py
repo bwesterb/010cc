bb6a4d26a5473c16354250ba41b4cc8cc4a70682
Bas Westerbaan <bas@westerbaan.name>
2014-03-28 11:08:28 +0100

    Another temp commit for work on server

--- server.py
+++ server.py
@@ -1,9 +1,24 @@
 import zmq
+import os.path
+import kyotocabinet
+
+BITS=100000
 
 class Server(object):
     def __init__(self):
         self.workers = {}
+    def open_db(self):
+        path = 'db.kch'
+        if os.path.exists(path):
+            self.db = kyotocabinet.db.open(path, kyotocabinet.DB.OWRITER)
+            return
+        self.db = kyotocabinet.db.open(path, kyotocabinet.DB.OWRITER |
+                                             kyotocabinet.DB.OCREATE)
+        for i in xrange(BITS):
+            self.db.set('todo-{}'.format(i), None)
+
     def main(self):
+        self.open_db()
         zctx = zmq.Context()
         s = zctx.socket(zmq.REP)
         s.bind('tcp://*:5555')
@@ -13,7 +28,6 @@ class Server(object):
                 s.send_json(['error', 'malformed request'])
                 continue
             if msg[0] == 'ping':
-                if 
                 s.send_json(['pong'])
                 continue
             s.send_json(['error', 'unknown message'])
